e54ede848e6fd20e70d471dde94563a38caee48d
Li Jinpei <jinpli@amd.com>
2026-05-01 00:00:42 +0800

    Load nvm from generated env

--- setup_dev_environment.sh
+++ setup_dev_environment.sh
@@ -117,6 +117,9 @@ config_env() {
 alias vim=nvim
 export PATH="$HOME/proot/nvim/bin:\$PATH"
 source $HOME/.local/bin/env
+export NVM_DIR="\$HOME/.nvm"
+# shellcheck disable=SC1091
+[ -s "\$NVM_DIR/nvm.sh" ] && \. "\$NVM_DIR/nvm.sh"
 BLOCK
 }
 
